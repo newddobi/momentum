bea1f87ebe71555d7c94fe1ae8076b397b09dd46
ddobi <qhdqhdekd261@naver.com>
2022-04-28 20:33:29 +0900

    first commit

---
+++ js/quotes.js
@@ -0,0 +1,45 @@
+const quotes = [
+  {
+    quote: "The most effective way to do it, is to do it.",
+    author: "Amelia Mary Earhart",
+  },
+  {
+    quote: "A goal without a plan is just a wish.",
+    author: "Saint-Exupery",
+  },
+  {
+    quote: "There are no menial jobs, only menial attitudes.",
+    author: "William J. Brennan Jr.",
+  },
+  {
+    quote:
+      "The world is moving so fast these days that the man who says it can't be done is generally interrupted by someone doing it.",
+    author: "Elbert Hubbard",
+  },
+  {
+    quote: "Done is better than perfect.",
+    author: "Sheryl Kara Sandberg",
+  },
+  {
+    quote: "All things are difficult before they are easy.",
+    author: "Thomas Fuller",
+  },
+  {
+    quote:
+      "Word hard. In the end, passion and hard work beats out natural talent.",
+    author: "Pete Docter",
+  },
+  {
+    quote: "The gratification comes in the doing, not in the results.",
+    author: "James Byron Dean",
+  },
+  {
+    quote: "Lead, follow or get out of the way.",
+    author: "Ted Turner",
+  },
+  {
+    quote:
+      "The world breaks everyone, and afterward, some are strong at the broken places.",
+    author: "Ernest Miller Hemingway",
+  },
+];
